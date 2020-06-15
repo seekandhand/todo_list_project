6c20282ecba8a7307072ee99631f12c0dc4af4b2
seekandhand <66113212+seekandhand@users.noreply.github.com>
2020-06-15 18:54:40 +0700

    imports minor refactoring

--- api/views.py
+++ api/views.py
@@ -1,15 +1,13 @@
 from django.contrib.auth import authenticate, login, logout
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.serializers import UserSerializer
+from api.serializers import UserSerializer, OrganizationSerializer, ToDoListSerializer
 from todo_lists.models import Organization, ToDoList
 from users.models import CustomUser
-from .serializers import OrganizationSerializer, ToDoListSerializer
 
 
 class RegisterView(CreateAPIView):
